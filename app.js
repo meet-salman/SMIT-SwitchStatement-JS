5d02eb091d3893043ec182c224138440c1732dc2
Salman Ahmed <say2salmanahmed@gmail.com>
2023-08-15 11:54:36 +0500

    Switch Statement

---
+++ app.js
@@ -0,0 +1,51 @@
+// var
+// let
+// const
+
+
+
+// let elderBrother = "Shaheer Ahmed";
+// const youngerBrother = "Salman Ahmed";
+
+// console.log(elderBrother , youngerBrother);
+
+// elderBrother = "Salman Ahmed";
+// console.log(elderBrother);
+
+// youngerBrother = "Shaheer Ahmed";
+// console.log(youngerBrother);
+
+
+
+// let num = 10;
+// switch (true) {
+//     case (num > 15):
+//         console.log("Pehla Case Chal Gya.");
+//         break;
+
+//     case (num < 20):
+//         console.log("Dusra Case Chal Gya.");
+//         break;
+
+//     default:
+//         console.log("Default Chal Gya.");
+//         break;
+// }
+
+
+
+// let num1 = 20;
+// switch (false) {
+//     case (num < 10):
+//         console.log("Pehla Case Chal Gya.");
+//         break;
+
+//     case (num < 15):
+//         console.log("Dusra Case Chal Gya.");
+//         break;
+
+//     default:
+//         console.log("Default Case Chal Gya.");
+//         break;
+// }
+
